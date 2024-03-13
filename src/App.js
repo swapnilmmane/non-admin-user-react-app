135445768d22fa8857763b1195240d3ed59e2684
Swapnil M Mane <swapnilmmane@gmail.com>
2024-03-14 01:11:53 +0530

    fix: tenant in header

--- src/App.js
+++ src/App.js
@@ -27,7 +27,8 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: accessToken ? `Bearer ${accessToken}` : ''
+      authorization: accessToken ? `Bearer ${accessToken}` : '',
+      'x-tenant': 'root'
     }
   };
 });
